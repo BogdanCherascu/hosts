d439f6a463612b574720f0d19a68b197c40d3970
Bogdan Cherascu <cherascubogdan@gmail.com>
2024-11-17 15:49:10 +0000

    Script pentru validarea adreselor IP din /etc/hosts

---
+++ verificare_host.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while read -r line; do
+	ip=$(echo $line | awk '{print $1}')
+	name=$(echo $line | awk '(print $2)')
+	if [ -n "$ip"] && [ -n "$name" ]; then
+		ip_bun=$(nslookup $name | grep 'Adress: ' | tail -n 1 | awk '{print $2}')
+		if [[ "$ip"  != "$ip_bun" ]]; then
+			echo "Bogus IP for $name in /etc/hosts!"
+		fi
+	fi
+done < /etc/hosts
+
